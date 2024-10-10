df57b0c9cf479885ad9bd1b8b87202aa5d5a52e0
André Igor <andreigorcosta@gmail.com>
2024-10-09 21:47:59 -0300

    Prod 1

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import { app } from './app'
 import { env } from '../env'
 
 
-app.listen({port: env.PORT}).then(()=>{
+app.listen({port: 10000}).then(()=>{
     console.log('servidor http rodando')
 })
